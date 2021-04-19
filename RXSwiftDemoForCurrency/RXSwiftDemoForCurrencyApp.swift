358e388b3fa65236654c1df4ab9094f7920369af
ashwinisapreknoxpo <ashwini.sapre@knoxpo.com>
2021-04-19 15:03:58 +0530

    Initial Commit

---
+++ RXSwiftDemoForCurrency/RXSwiftDemoForCurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RXSwiftDemoForCurrencyApp.swift
+//  RXSwiftDemoForCurrency
+//
+//  Created by Knoxpo MacBook Pro on 19/04/21.
+//
+
+import SwiftUI
+
+@main
+struct RXSwiftDemoForCurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
